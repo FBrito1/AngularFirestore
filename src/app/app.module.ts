0a7633e13ee98f0636a985da90c580a95864a43e
Fabio Brito <luan.tecno@gmail.com>
2017-10-18 13:32:35 -0200

    blog app versão 1.0.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
+
+var firebaseConfig =  {
+  apiKey: "AIzaSyDY7oiFRTTjKwIVZmVtOulL6eI2PL6S-vo",
+  authDomain: "firestoreangular.firebaseapp.com",
+  databaseURL: "https://firestoreangular.firebaseio.com",
+  projectId: "firestoreangular",
+  storageBucket: "",
+  messagingSenderId: "615740431094"
+};
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
